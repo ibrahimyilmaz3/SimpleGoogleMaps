bb9ff5a09084e5c90ebfffc90a6d1facd819cfaf
arcticdeveloper1 <ibrahimyilmaz99999@gmail.com>
2022-08-04 19:03:27 +0300

    SimpleGoogleMapsAPI

---
+++ app/src/main/java/com/iyilmaz/simplegooglemaps/models/Place.kt
@@ -0,0 +1,5 @@
+package com.iyilmaz.simplegooglemaps.models
+
+import java.io.Serializable
+
+data class Place(val title:String, val description: String, val latitude: Double, val longitude: Double) : Serializable
